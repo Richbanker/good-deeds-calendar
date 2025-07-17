3c30066558a987e875ed51988d83345f2513eb2c
registration <gajienko44@gmail.com>
2025-07-17 22:08:55 +0300

    Обновление проекта: последние изменения

--- src/components/AddDeedModal.tsx
+++ src/components/AddDeedModal.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 import type { GoodDeed } from "../store/useAppStore";
 
 interface AddDeedModalProps {
